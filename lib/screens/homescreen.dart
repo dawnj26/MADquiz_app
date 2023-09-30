341856b52cb35bc474f4a2397f8e8da8fb8571f0
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2023-09-30 16:16:08 +0800

    add home screen

---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/components/question.dart';
+import 'package:quiz_app/components/welcome.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isPlay = false;
+
+  void startGame() {
+    setState(() {
+      isPlay = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red.shade100,
+      appBar: AppBar(
+        title: const Text('Quiz App'),
+        backgroundColor: Colors.red.shade300,
+      ),
+      body: isPlay ? const Question() : Welcome(startGame),
+    );
+  }
+}
